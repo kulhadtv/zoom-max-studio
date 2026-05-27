8e8710958c7fd558e604906bb89f2a759f6b03bc
nirakantaarts <nirakantaarts@gmail.com>
2026-05-27 13:40:37 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Router from './router/router'
 import Navbar from './components/common/Navbar'
 import Footer from './components/common/Footer'
+import Router from './router/Router'
 
 const App = () => {
   return (
